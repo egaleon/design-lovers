eb51a33eba5f9fd5bc5583fd2cd2b11c7f090ee8
egaleon <egab.leon@gmail.com>
2026-03-13 22:32:50 -0500

    chore: luxury initialisation and theme

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'dl-ivory': '#F9F7F2',
+        'dl-gold': '#C5A059',
+        'dl-coffee': '#3D3630',
+        'dl-champagne': '#E8DED1',
+      },
+      fontFamily: {
+        serif: ['Cormorant Garamond', 'serif'],
+        sans: ['Montserrat', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
